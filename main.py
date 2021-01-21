00c7dc589d472ac464512c4d6dd5ed797a61e0e4
James Smith <theworld969@hotmail.com>
2021-01-21 14:38:23 -0700

    Change to postgresql in production

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'DATABASE_URL', "sqlite://blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
